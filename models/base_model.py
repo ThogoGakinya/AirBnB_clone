3e5d0facfcbc08fdf511a7d35df5df93f6e9fc8e
Kingzbrian <murageb254@gmail.com>
2023-02-12 09:43:23 -0800

    "corrections"

--- models/base_model.py
+++ models/base_model.py
@@ -24,14 +24,14 @@ class BaseModel:
 
         """
         if (kwargs):
-                for key, value in kwargs.items():
-                    if key != "__class__":
-                        if key == "created_at" or key == "updated_at":
-                            val = datetime.strptime(value,
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        val = datetime.strptime(value,
                                                     '%Y-%m-%dT%H:%M:%S.%f')
-                            setattr(self, key, val)
-                        else:
-                            setattr(self, key, value)
+                        setattr(self, key, val)
+                    else:
+                        setattr(self, key, value)
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
